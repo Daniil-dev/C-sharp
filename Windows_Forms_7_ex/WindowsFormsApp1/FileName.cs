510a1f56fbab434799f425cd803b5f1e3e18a047
Daniil-dev <60258376+Daniil-dev@users.noreply.github.com>
2020-06-27 10:29:43 +0300

    Add files via upload

---
+++ Windows_Forms_7_ex/WindowsFormsApp1/FileName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FileName : Form
+    {
+        public string fileName = "";
+
+        public FileName()
+        {
+            InitializeComponent();
+        }
+
+        private void FileName_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            fileName = textBox1.Text;
+        }
+    }
+}
